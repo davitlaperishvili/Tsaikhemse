1c6804a1e08a774202a20c6a6fe62e7127a21002
davitlaperishvili <davitlaperishvili@gmail.com>
2019-11-12 14:11:02 +0400

    responsive and Menu-Button update

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,28 @@ for (let i = 0; i < bl; i++){
          this.textContent = "დამატება";
       } 
    });
-   
+}
 
+let menu_button = document.getElementsByClassName('MiniMenu');
+//console.log(menu_button);
+let item = menu_button.length;
+//console.log(item);
+let open_menu = document.getElementsByClassName('nav');
+let menu = open_menu.length;
+//console.log(menu);
+// function OpenMenu() {
+//    document.body.children[0].children[1].classList.toggle('open');
+// }
 
+for(let i = 0; i < item; i++){
+   //console.log(i);'
+   menu_button[i].addEventListener('click', function() {
+      document.body.children[0].children[1].classList.toggle('open');
+   });
+}
+
+// document.getElementById("MiniMenu").addEventListener("click", displayOpen);
+
+// function displayOpen() {
+//     document.getElementsByClassName("nav").classList.toggle('open');
+// }
